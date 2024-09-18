ba724fe7302a916072d9b6d29b8877f92b8d66ac
Malika <144811887+a-malika@users.noreply.github.com>
2024-09-18 21:24:54 +0600

    Add files via upload

---
+++ task4.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//4 задание
+//Dependency-Inversion Principle
+namespace Laboratory4
+{
+    public interface INotification
+    {
+        void Send(string message);
+    }
+    public class EmailService : INotification
+    {
+        public void Send(string message)
+        {
+            Console.WriteLine($"Отправка Email: {message}");
+        }
+    }
+    public class NotificationService
+    {
+        private INotification Notification;
+        public NotificationService(INotification notification)
+        {
+            Notification = notification;
+        }
+        public void Send(string message)
+        {
+            Notification.Send(message);
+        }
+    }
+}
